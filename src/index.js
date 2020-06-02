340e266756546b5296ca5976c6758a9b86ce5b99
sangeeky <sangeeky@outlook.com>
2020-06-02 10:31:11 -0500

    First Page with Navbar Hero and Badge

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from "react-dom"; // ApppendChild
 import "bootstrap/dist/css/bootstrap.css";
 import "./components/global.css";
 
-import Badge from "./components/Badge";
+import BadgePage from "./pages/BadgePage";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render(que,donde)
-ReactDOM.render(
-  <Badge
-    firstname="Julian"
-    lastname="Vallejo"
-    avatar="https://www.gravatar.com/avatar/ae0b876156856ac9e18841a51b8fb62b"
-    jobTitle="Fisico Duro"
-    twitter="pinpacho"
-  />,
-  container
-);
+ReactDOM.render(<BadgePage />, container);
---
+++ src/pages/BadgePage.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+
+import header from "../images/badge-header.svg";
+import "./styles/BadgeNew.css";
+
+class BadgePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstname="Santiago"
+                lastname="Pinchao"
+                twitter="SanGeeky"
+                avatar="https://www.gravatar.com/avatar/ae0b876156856ac9e18841a51b8fb62b"
+                jobtitle="Student Duro"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgePage;
